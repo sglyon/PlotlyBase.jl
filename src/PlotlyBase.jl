1f9897390693024c3e628d256ed5992de9569ca9
Spencer Lyon <spencerlyon2@gmail.com>
2018-01-23 10:22:29 -0500

    INIT: move stuff over from PlotlyJS

--- src/PlotlyBase.jl
+++ src/PlotlyBase.jl
@@ -1,5 +1,107 @@
+__precompile__()
+
 module PlotlyBase
 
-# package code goes here
+using Base.Iterators
+using JSON
+using DocStringExtensions
+using Requires
+
+import Base: ==
+
+# import LaTeXStrings and export the handy macros
+using LaTeXStrings
+export @L_mstr, @L_str
+
+# export some names from JSON
+export json
+
+_symbol_dict(x) = x
+_symbol_dict(d::Associative) =
+    Dict{Symbol,Any}([(Symbol(k), _symbol_dict(v)) for (k, v) in d])
+
+# include these here because they are used below
+include("traces_layouts.jl")
+include("styles.jl")
+
+# core plot object
+mutable struct Plot{TT<:AbstractTrace}
+    data::Vector{TT}
+    layout::AbstractLayout
+    divid::Base.Random.UUID
+    style::Style
+end
+
+function Base.show(io::IO, ::MIME"text/plain", p::Plot)
+    println(io, """
+    data: $(json(map(_describe, p.data), 2))
+    layout: "$(_describe(p.layout))"
+    """)
+end
+
+Base.show(io::IO, p::Plot) = show(io, MIME("text/plain"), p)
+
+# include the rest of the core parts of the package
+include("util.jl")
+include("json.jl")
+include("subplots.jl")
+include("api.jl")
+include("convenience_api.jl")
+@require DataFrames include("dataframes_api.jl")
+include("recession_bands.jl")
+
+# Set some defaults for constructing `Plot`s
+function Plot(;style::Style=CURRENT_STYLE[])
+    Plot(GenericTrace{Dict{Symbol,Any}}[], Layout(), Base.Random.uuid4(), style)
+end
+
+function Plot{T<:AbstractTrace}(data::AbstractVector{T}, layout=Layout();
+                                style::Style=CURRENT_STYLE[])
+    Plot(data, layout, Base.Random.uuid4(), style)
+end
+
+function Plot(data::AbstractTrace, layout=Layout();
+              style::Style=CURRENT_STYLE[])
+    Plot([data], layout; style=style)
+end
+
+
+# NOTE: we export trace constructing types from inside api.jl
+# NOTE: we export names of shapes from traces_layouts.jl
+export
+
+    # core types
+    Plot, GenericTrace, Layout, Shape, AbstractTrace, AbstractLayout,
+
+    # plotly.js api methods
+    restyle!, relayout!, update!, addtraces!, deletetraces!, movetraces!,
+    redraw!, extendtraces!, prependtraces!, purge!, to_image, download_image,
+
+    # non-!-versions (forks, then applies, then returns fork)
+    restyle, relayout, update, addtraces, deletetraces, movetraces, redraw,
+    extendtraces, prependtraces,
+
+    # helper methods
+    plot, fork, vline, hline, attr,
+
+    # new trace types
+    stem,
+
+    # convenience stuff
+    add_recession_bands!,
+
+    # frontend methods
+    init_notebook,
+
+    # styles
+    use_style!, style, Style, Cycler
+
+@init begin
+    env_style = Symbol(get(ENV, "PLOTLYJS_STYLE", ""))
+    if env_style in STYLES
+        global DEFAULT_STYLE
+        DEFAULT_STYLE[] = Style(env_style)
+    end
+end
 
 end # module
