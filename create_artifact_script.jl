a431cb7b7bc7a8d5a2fda8443e62c8a0e3a60f37
Spencer Lyon <spencerlyon2@gmail.com>
2020-08-11 12:49:53 -0400

    ENH: integrate kaleido to save/export figures

---
+++ create_artifact_script.jl
@@ -0,0 +1,82 @@
+using Pkg.Artifacts, ghr_jll
+using Pkg.BinaryPlatforms
+import ZipFile
+
+release_tag = "v0.3.1"
+
+artifact_toml = joinpath(@__DIR__, "Artifacts.toml")
+
+function unzip(zf_path, dest_path)
+    r = ZipFile.Reader(zf_path)
+    k_path = joinpath(dest_path, "kaleido")
+    rm(k_path , force=true, recursive=true)
+    mkdir(k_path)
+    for f in r.files
+        out_path = joinpath(dest_path, f.name)
+        if (endswith(f.name, "/") || endswith(f.name, "\\")) && !(isdir(f.name))
+            mkdir(out_path)
+        else
+            parent_dir = joinpath(dest_path, dirname(f.name))
+            if !isdir(parent_dir)
+                mkpath(parent_dir)
+            end
+            v = Vector{UInt8}(undef, f.uncompressedsize)
+            read!(f, v)
+            write(out_path, v)
+        end
+    end
+    close(r)
+end
+
+_download_os(::Linux) = "linux"
+_download_os(::MacOS) = "mac"
+_download_os(::Windows) = "win"
+
+function _download_os()
+    if Sys.isunix()
+        return Sys.isapple() ? "mac" : "linux"
+    end
+    if Sys.iswindows()
+        return "win"
+    end
+    error("Only mac, linux, and windows are supported")
+end
+
+function download_kaleido(k_dir, os=_download_os())
+    url = "https://github.com/plotly/Kaleido/releases/download/v0.0.3/kaleido_$os-0.0.3.zip"
+    dest = joinpath(k_dir, "kaleido.zip")
+    download(url, dest)
+    unzip(dest, k_dir)
+    rm(dest)  # remove zip file
+    return
+end
+
+for platform in [Windows(:x86_64),]
+    kaleido_hash = artifact_hash("kaleido", artifact_toml, platform=platform)
+    # If the name was not bound, or the hash it was bound to does not exist, create it!
+    if kaleido_hash === nothing || !artifact_exists(kaleido_hash)
+        # create_artifact() returns the content-hash of the artifact directory once we're finished creating it
+        kaleido_hash = create_artifact() do k_dir
+            download_kaleido(k_dir, _download_os(platform))
+        end
+    end
+
+    local tarball_hash
+    gz_name = "kaleido_$(_download_os(platform)).tar.gz"
+    mktempdir() do dir
+        gz_path = joinpath(dir, gz_name)
+        tarball_hash = archive_artifact(kaleido_hash, gz_path)
+        ghr() do ghr
+            run(`$(ghr) -u sglyon -r PlotlyBase.jl $(release_tag) $(gz_path)`)
+        end
+    end
+    bind_artifact!(
+        artifact_toml,
+        "kaleido",
+        kaleido_hash;
+        platform=platform,
+        download_info = [
+            ("https://github.com/sglyon/PlotlyBase.jl/releases/download/$(release_tag)/$(gz_name)", tarball_hash),
+        ],
+    )
+end
---
+++ src/kaleido.jl
@@ -0,0 +1,151 @@
+const BIN = `$(joinpath(artifact"kaleido", "kaleido", "kaleido")) plotly --disable-gpu`
+
+mutable struct Pipes
+    stdin::Pipe
+    stdout::Pipe
+    stderr::Pipe
+    proc::Base.Process
+    Pipes() = new()
+end
+
+const P = Pipes()
+
+const ALL_FORMATS = ["png", "jpg", "jpeg", "webp", "svg", "pdf", "eps", "json"]
+const TEXT_FORMATS = ["svg", "json", "eps"]
+
+function _restart_kaleido_process()
+    if process_running(P.proc)
+        kill(P.proc)
+    end
+    _start_kaleido_process()
+end
+
+
+function _start_kaleido_process()
+    global P
+    try
+        kstdin = Pipe()
+        kstdout = Pipe()
+        kstderr = Pipe()
+        kproc = run(pipeline(BIN,
+                             stdin = kstdin, stdout = kstdout, stderr = kstderr),
+                    wait = false)
+        process_running(kproc) || error("There was a problem startink up kaleido.")
+        close(kstdout.in)
+        close(kstderr.in)
+        close(kstdin.out)
+        Base.start_reading(kstderr.out)
+        P.stdin = kstdin
+        P.stdout = kstdout
+        P.stderr = kstderr
+        P.proc = kproc
+
+        # read startup message and check for errors
+        res = readline(P.stdout)
+        if length(res) == 0
+            error("Could not start Kaledio process")
+        end
+
+        js = JSON.parse(res)
+        if get(js, "code", 0) != 0
+            error("Could not start Kaledio process")
+        end
+    catch
+        @warn "Kaledio is not available on this system. Julia will be unable to produce any plots."
+    end
+    nothing
+end
+
+function savefig(
+        p::Plot;
+        width::Union{Nothing,Int}=nothing,
+        height::Union{Nothing,Int}=nothing,
+        scale::Union{Nothing,Real}=nothing,
+        format::String="png"
+    )::Vector{UInt8}
+    if !(format in ALL_FORMATS)
+        error("Unknown format $format. Expected one of $ALL_FORMATS")
+    end
+
+    # construct payload
+    _get(x, def) = x === nothing ? def : x
+    payload = Dict(
+        :width => _get(width, 700),
+        :height => _get(height, 500),
+        :scale => _get(scale, 1),
+        :format => format,
+        :data => p
+    )
+
+    _ensure_kaleido_running()
+
+    # convert payload to vector of bytes
+    bytes = transcode(UInt8, JSON.json(payload))
+    write(P.stdin, bytes)
+    write(P.stdin, transcode(UInt8, "\n"))
+    flush(P.stdin)
+
+    # read stdout and parse to json
+    res = readline(P.stdout)
+    js = JSON.parse(res)
+
+    # check error code
+    code = get(js, "code", 0)
+    if code != 0
+        msg = get(js, "message", nothing)
+        error("Transform failed with error code $code: $msg")
+    end
+
+    # get raw image
+    img = String(js["result"])
+
+    # base64 decode if needed, otherwise transcode to vector of byte
+    if format in TEXT_FORMATS
+        return transcode(UInt8, img)
+    else
+        return base64decode(img)
+    end
+end
+
+function savefig(io::IO,
+        p::Plot;
+        width::Union{Nothing,Int}=nothing,
+        height::Union{Nothing,Int}=nothing,
+        scale::Union{Nothing,Real}=nothing,
+        format::String="png")
+
+    format == "html" && return savehtml(io, p)
+
+    bytes = savefig(p, width=width, height=height, scale=scale, format=format)
+    write(io, bytes)
+end
+
+"""
+    savefig(p::Plot, fn::AbstractString; format=nothing, scale=nothing,
+    width=nothing, height=nothing)
+Save a plot `p` to a file named `fn`. If `format` is given and is one of
+(png, jpeg, webp, svg, pdf, eps), it will be the format of the file. By
+default the format is guessed from the extension of `fn`. `scale` sets the
+image scale. `width` and `height` set the dimensions, in pixels. Defaults
+are taken from `p.layout`, or supplied by plotly
+"""
+function savefig(
+        p::Plot, fn::AbstractString;
+        format::Union{Nothing,String}=nothing,
+        width::Union{Nothing,Int}=nothing,
+        height::Union{Nothing,Int}=nothing,
+        scale::Union{Nothing,Real}=nothing,
+    )
+    ext = split(fn, ".")[end]
+    if format === nothing
+        format = String(ext)
+    end
+
+    open(fn, "w") do f
+        savefig(f, p; format=format, scale=scale, width=width, height=height)
+    end
+    return fn
+end
+
+_kaleido_running() = isopen(P.stdin) && process_running(P.proc)
+_ensure_kaleido_running() = !_kaleido_running() && _restart_kaleido_process()
