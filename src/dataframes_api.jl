2c9d5d051563f278e2b6ba747b6429d48e1af1b2
Rudolf Sailer <Rudolf.Sailer@mercateo.com>
2020-11-19 11:45:50 +0100

    Change by to groupby to reflect API Change with DataFrames

--- src/dataframes_api.jl
+++ src/dataframes_api.jl
@@ -38,10 +38,8 @@ different mean for each group.
 function GenericTrace(df::DataFrames.AbstractDataFrame; group=nothing, kind="scatter", kwargs...)
     d = Dict{Symbol,Any}(kwargs)
     if _has_group(df, group)
-        _traces = DataFrames.by(df, group) do dfg
-            GenericTrace(dfg; kind=kind, name=_group_name(dfg, group), kwargs...)
-        end
-        return GenericTrace[t for t in _traces[:x1]]
+        _traces = map(dfg -> GenericTrace(dfg; kind=kind, name=_group_name(dfg, group), kwargs...), DataFrames.groupby(df, group))
+        return GenericTrace[t for t in _traces]
     else
         if (group !== nothing)
             @warn "Unknown group $(group), skipping"
