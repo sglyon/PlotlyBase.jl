fec63ca5bc88df8d1a9816ee3dc1e3fed854f420
hhaensel <31985040+hhaensel@users.noreply.github.com>
2022-01-12 10:49:03 +0100

    enhancing json3 for traces
    This is a PR referring to #88

--- src/json3.jl
+++ src/json3.jl
@@ -8,3 +8,6 @@ const StructTypes = JSON3.StructTypes
 
 StructTypes.StructType(::Type{<:PlotlyBase.Plot}) = JSON3.RawType()
 JSON3.rawbytes(plot::PlotlyBase.Plot) = codeunits(JSON.json(plot))
+
+StructTypes.StructType(::Type{<:PlotlyBase.AbstractTrace}) = JSON3.RawType()
+JSON3.rawbytes(trace::PlotlyBase.AbstractTrace) = codeunits(JSON.json(trace))
