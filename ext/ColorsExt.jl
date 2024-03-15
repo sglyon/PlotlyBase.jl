37486ddfa6f49d62ff9c9d30bae177ec5dad7cbe
Helmut Hänsel <helmut.haensel@gmx.com>
2024-03-15 20:06:12 +0100

    fix json for Layout and Colorant, add tests

--- ext/ColorsExt.jl
+++ ext/ColorsExt.jl
@@ -3,6 +3,6 @@ module ColorsExt
 isdefined(Base, :get_extension) ? (using Colors) : (using ..Colors)
 using PlotlyBase
 
-_json_lower(a::Colors.Colorant) = string("#", Colors.hex(a))
+PlotlyBase._json_lower(a::Colorant) = "#$(hex(a, :auto))"
 
 end
