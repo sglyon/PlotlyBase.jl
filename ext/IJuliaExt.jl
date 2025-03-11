a5eb88fb56458dabe1aa550df4373c1e3006b062
Alberto Mengali <disberd@gmail.com>
2025-03-11 09:43:19 +0100

    fix IJuliaExt

--- ext/IJuliaExt.jl
+++ ext/IJuliaExt.jl
@@ -5,11 +5,11 @@ using PlotlyBase
 
 function IJulia.display_dict(p::Plot)
     Dict(
-        "application/vnd.plotly.v1+json" => JSON.lower(p),
+        "application/vnd.plotly.v1+json" => PlotlyBase.JSON.lower(p),
         "text/plain" => sprint(show, "text/plain", p),
         "text/html" => let
             buf = IOBuffer()
-            show(buf, MIME("text/html"), p, include_plotlyjs="require")
+            show(buf, MIME("text/html"), p)
             String(take!(buf))
         end
     )
