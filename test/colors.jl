40fee9f40ee6043a505560dc029a62edca896af6
Helmut Hänsel <helmut.haensel@gmx.de>
2025-10-14 17:03:32 +0200

    bump compat of JSON to include v1, adapt one test to match all versions

--- test/colors.jl
+++ test/colors.jl
@@ -1,5 +1,8 @@
 using Colors
 using PlotlyBase.JSON
+
+import PlotlyBase.JSON.json
+
 @testset "Colors" begin
     @test JSON.parse(json(Layout(colortest = RGBA(1, 0.8, 0.6, 0.2))))["colortest"] == "#FFCC9933"
     @test JSON.parse(json(Template(data = attr(scatter = [attr(marker_color = colorant"blue")]))))["data"]["scatter"][1]["marker"]["color"] == "#0000FF"
